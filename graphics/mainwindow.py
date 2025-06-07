ab274d3c9a539e218a11f5d67a3568937f59c839
Coalyni <coalyni34work@proton.me>
2025-06-08 01:33:32 +0300

    added an icon

--- graphics/mainwindow.py
+++ graphics/mainwindow.py
@@ -5,7 +5,7 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(275, 223)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("Flag_of_Interslavic.svg.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("inter.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
